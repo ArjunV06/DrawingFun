c32e8031e52dd9f371284549affd6719ce6d779f
LifesMystery <arjunvsudn@gmail.com>
2022-10-31 09:13:46 -0500

    first commit

---
+++ DrawingFun.java
@@ -0,0 +1,11 @@
+public class DrawingFun
+{
+    public static void main(String[] args)
+    {
+        System.out.println("U    U  III  L       CCCCCC  SSSSSS");
+        System.out.println("U    U   I   L       C       S");
+        System.out.println("U    U   I   L       C       SSSSSS");
+        System.out.println("U    U   I   L       C            S");
+        System.out.println("UUUUUU  III  LLLLLL  CCCCCC  SSSSSS");
+    }
+}
